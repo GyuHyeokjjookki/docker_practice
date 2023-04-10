3135cac2091ab114f4a571e33a9af17636c38c24
gyu_hyeok <jjookki33@gmail.com>
2023-04-10 10:31:39 +0900

    modified docker with Django

--- docker_server_Django/mysite/settings.py
+++ docker_server_Django/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$&*iff98k4fcg^m%l)^rs+!fjy2r=z2rq$1^t=yc)jd(*afa3a'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
